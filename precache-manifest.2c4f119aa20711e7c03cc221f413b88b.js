1061aeae840d5f354abaace6e5cfdf943f541105
Kasun Ranasinghe <khasitharanasinghe@gmail.com>
2023-08-28 02:00:10 +0530

    Updates

---
+++ precache-manifest.2c4f119aa20711e7c03cc221f413b88b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdbfa96eb3d0d5a8d5dd",
+    "url": "/static/css/main.9548f97f.chunk.css"
+  },
+  {
+    "revision": "bdbfa96eb3d0d5a8d5dd",
+    "url": "/static/js/main.bdbfa96e.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "11a3eb75ba923fb4e350",
+    "url": "/static/js/2.11a3eb75.chunk.js"
+  },
+  {
+    "revision": "439225c8a17b56244b87",
+    "url": "/static/js/runtime~main.439225c8.js"
+  },
+  {
+    "revision": "3b0de23363ad70579a14c76aeedbb87e",
+    "url": "/index.html"
+  }
+];
